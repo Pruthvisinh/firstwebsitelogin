04bab7adaef7ffc8a02f2deeefc39c7cb6f68ee6
Pruthvisinh <prtvsinh@gmail.com>
2020-05-26 17:02:51 +0530

    change time-zone and config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Firstwebsite
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     #config.time_zone = 'Kolkata'
+    config.time_zone = 'Kolkata'
   end
 end
